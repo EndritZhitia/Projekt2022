618ed53a211801cc2f1fb6593f2fd6975680b607
Admin <Admin@DESKTOP-5KQQGBE>
2022-12-23 23:10:48 +0100

    Add project files.

---
+++ Ecommerce2022/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce2022.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce2022.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Ecommerce2022/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace Ecommerce2022.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Type { get; set; }
+
+        public int Barcode { get; set; }
+
+        public string Country { get; set; }
+        public DateTime ProductionDate { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+
+
+    }
+}
